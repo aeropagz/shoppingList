d49ac359fd5e84dde78f259ea65082c4c2a1618f
Klaas Pelzer <klaas.pelzer@gmx.de>
2021-01-05 11:11:21 +0000

    https added

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,14 @@
 const app = require('./app');
+const https = require('https');
+const fs = require('fs');
 
 const port = process.env.PORT || 8080;
-app.listen(port, () => {
-    console.log("Server started at port: " + port);
-  });
+
+https.createServer({
+	key: fs.readFileSync('/etc/letsencrypt/live/simplelist.de-0001/privkey.pem'),
+	cert: fs.readFileSync('/etc/letsencrypt/live/simplelist.de-0001/cert.pem'),
+	ca: fs.readFileSync('/etc/letsencrypt/live/simplelist.de-0001/chain.pem')
+	},app)
+	.listen(port, () => {
+		console.log(`Server started on Port ${port}`);
+	});
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+production: true,
+apiUrl: 'https://simplelist.de:8080'
 };
