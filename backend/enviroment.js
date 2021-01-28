33165b4a143928263417de8ba3a98a60d4cc5d33
Klaas <klaas.pelzer@gmx.de>
2021-01-28 15:47:09 +0100

    implemented env var for express prod

--- backend/enviroment.js
+++ backend/enviroment.js
@@ -1,7 +1,7 @@
 let enviroment = {
   prodURL: "https://simplelist.de",
   devURL: "http://192.168.50.47:4200",
-  production: false,
+  production: process.env.EXPRESSPROD || null,
 };
 enviroment.frontUrl = enviroment.production
   ? enviroment.prodURL
